7970aafb12e56da54d711f77c7341311ef02b94c
Aryan Garg <aryan.garg_cs18@gla.ac.in>
2021-02-19 19:52:15 +0530

    complete work

---
+++ JavaScript Files/calculator.js
@@ -0,0 +1,29 @@
+  
+        function Add()
+        {
+            var value1=document.getElementById('val1').value;
+            var value2=document.getElementById('val2').value; 
+            document.getElementById('result').value=parseInt(value1)+parseInt(value2);  
+            
+        }
+        function Sub()
+        {
+            var value1=document.getElementById('val1').value;
+            var value2=document.getElementById('val2').value; 
+            document.getElementById('result').value=parseInt(value1)-parseInt(value2);  
+            
+        }
+        function Mul()
+        {
+            var value1=document.getElementById('val1').value;
+            var value2=document.getElementById('val2').value; 
+            document.getElementById('result').value=parseInt(value1)*parseInt(value2);  
+            
+        }
+        function div()
+        {
+            var value1=document.getElementById('val1').value;
+            var value2=document.getElementById('val2').value; 
+            document.getElementById('result').value=parseInt(value1)/parseInt(value2);  
+            
+        }
